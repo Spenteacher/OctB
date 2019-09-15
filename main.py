f2fadeb49f390590f0d82cf03cb8e3f560224fbe
Spenteacher <samzaragoza.vcia@gmail.com>
2019-09-15 11:09:55 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,111 @@
+final = 10
+while final == 10:
+    texto = input("Introduce la letra > ")
+    a = ("a")
+    b = ("b")
+    c = ("c")
+    d = ("d")
+    e = ("e")
+    f = ("f")
+    g = ("g")
+    h = ("h")
+    i = ("i")
+    j = ("j")
+    k = ("k")
+    l = ("l")
+    m = ("m")
+    n = ("n")
+    ñ = ("ñ")
+    o = ("o")
+    p = ("p")
+    q = ("q")
+    r = ("r")
+    s = ("s")
+    t = ("t")
+    u = ("u")
+    v = ("v")
+    w = ("w")
+    x = ("x")
+    y = ("y")
+    z = ("z")
+    if texto == a:
+        a = ("032")
+        print(" Tu letra codificada > " + a)
+    if texto == b:
+        b = ("031")
+        print(" Tu letra codificada > " + b)
+    if texto == c:
+        c = ("030")
+        print(" Tu letra codificada > " + c)
+    if texto == d:
+        d = ("027")
+        print(" Tu letra codificada > " + d)
+    if texto == e:
+        e = ("026")
+        print(" Tu letra codificada > " + e)
+    if texto == f:
+        f = ("025")
+        print(" Tu letra codificada > " + f)
+    if texto == g:
+        g = ("024")
+        print(" Tu letra codificada > " + g)
+    if texto == h:
+        h = ("023")
+        print(" Tu letra codificada > " + h)
+    if texto == i:
+        i = ("022")
+        print(" Tu letra codificada > " + i)
+    if texto == j:
+        j = ("021")
+        print(" Tu letra codificada > " + j)
+    if texto == k:
+        k = ("020")
+        print(" Tu letra codificada > " + k)
+    if texto == l:
+        l = ("017")
+        print(" Tu letra codificada > " + l)
+    if texto == m:
+        m = ("016")
+        print(" Tu letra codificada > " + m)
+    if texto == n:
+        n = ("015")
+        print(" Tu letra codificada > " + n)
+    if texto == ñ:
+        ñ = ("014")
+        print(" Tu letra codificada > " + ñ)
+    if texto == o:
+        o = ("013")
+        print(" Tu letra codificada > " + o)
+    if texto == p:
+        p = ("012")
+        print(" Tu letra codificada > " + p)
+    if texto == q:
+        q = ("011")
+        print(" Tu letra codificada > " + q)
+    if texto == r:
+        r = ("010")
+        print(" Tu letra codificada > " + r)
+    if texto == s:
+        s = ("007")
+        print(" Tu letra codificada > " + s)
+    if texto == t:
+        t = ("006")
+        print(" Tu letra codificada > " + t)
+    if texto == u:
+        u = ("005")
+        print(" Tu letra codificada > " + u)
+    if texto == v:
+        v = ("004")
+        print(" Tu letra codificada > " + v)
+    if texto == w:
+        w = ("003")
+        print(" Tu letra codificada > " + w)
+    if texto == x:
+        x = ("002")
+        print(" Tu letra codificada > " + x)
+    if texto == y:
+        y = ("001")
+        print(" Tu letra codificada > " + y)
+    if texto == z:
+        z = ("000")
+        print(" Tu letra codificada > " + z)        
